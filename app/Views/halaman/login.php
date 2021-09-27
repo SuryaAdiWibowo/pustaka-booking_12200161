ef99bf4e52faa798a89fa2a33871f4f5fe8e1d5c
SuryaAdiWibowo <12200161@bsi.ac.id>
2021-09-27 12:04:44 +0700

    tugas pertemuan 2

---
+++ app/Views/halaman/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Beranda Login</title>
+</head>
+<body>
+    <h1>Selamat datang di sistem informasi perpustakaan</h1>
+    <p>Ini adalah layanan perpustakaan digital masa kini  
+        <a href="<?=base_url( relativePath:'/daftar-member')?>">klik daftar</a>untuk mendaftar menjadi member
+    </p>
+    
+</body>
+</html>
