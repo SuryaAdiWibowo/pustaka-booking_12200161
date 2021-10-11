a66275f9adc34edd50457bad5adf10f91aff61ad
SuryaAdiWibowo <12200161@bsi.ac.id>
2021-10-11 15:05:28 +0700

    Tugas Pertemuan 4

---
+++ app/Models/PenggunaModel_12200161.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class PenggunaModel extends Model
+{
+    protected $DBGroup              = 'koneksiku';
+    protected $table                = 'pengguna';
+    protected $primaryKey           = 'id';
+    protected $useAutoIncrement     = true;
+    protected $returnType           = 'array';
+    protected $protectFields        = true;
+    protected $allowedFields        = ['nama', 'password'];
+
+    
+    
+}
